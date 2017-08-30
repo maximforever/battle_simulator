08322e8d218d0f947cd27962823eaf4e910a43e7
pekarsky.maxim@gmail.com <flufci@gmail.com>
2017-08-30 15:44:05 -0400

    refactor code, improve styling

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
     Process:
 
     1. Decide on how many archers and footmen each player has, and adjust the base stats for these units
-    2. Each created unit gets added to the "allTheUnits" array once per point of speed. Faster units are more likely to attack, so they get added more times. 
+    2. Each created unit ID gets added to the "allTheUnits" array once per point of speed. Faster units are more likely to attack, so they get added more times. 
        Ex: Archers have a speed of 8; add 8 times.
     2. Each created unit also gets added once to either a p1 or a p2 array. These arrays keep track of which units are still alive.
     3. Draw a unit from the allTheUnits array. Units with more speed are more common, and more likely to get picked. This is our attacking unit.
@@ -35,7 +35,6 @@ var runs = 10;                      // this is how many times we want to simulat
 var completion;                     // this will help us track what % of the battles are completed (helpful for many runs)
 
 var running = false;                // this prevents the simulation from being run multiple times.
-
 
 /* SETUP AND UNIT DATA - edit this for playtesting */
 
@@ -138,10 +137,15 @@ function assignUnits(){                                                         
 
     for(var i = 0; i < unitContainer.length; i++){                                  // for each unit in the unitContainer array
 
-        for(var j = 0; j < unitContainer[i].speed; j++){                            // push this unit into the allTheUnits array once per point of speed (faster units are more frequent)
-            allTheUnits.push(JSON.parse(JSON.stringify(unitContainer[i])));         /* !! we can't simply push(unitContainer[i]), because this creates a "shallow" copy - it references the same object as the unitContainer arrayx f */
-        }                                                                           /* The whole of point of these temporary arrays is to work with copies of objects - so we must */
-                                                                                    /* stringify and parse to create independent object copies */
+
+        for(var j = 0; j < unitContainer[i].speed; j++){                            // push this unit into the allTheUnits array once per point of speed (since faster units should be picked more often)
+            allTheUnits.push(unitContainer[i].id);                                  // we can push just the unique unit ID into this array (no need to clone the whole unit, which is slow);
+        }
+
+        /*      For the following two arrays, we can't simply push(unitContainer[i]), because this creates a "shallow" copy, 
+                which references the same object as the unitContainer arrayx f The whole of point of these temporary arrays 
+                is to work with copies of objects - so we must stringify and parse to create independent object copies 
+        */
 
         if(unitContainer[i].player == 1){                                           // if this unit belongs to Player 1, add it to p1[] once
             p1.push(JSON.parse(JSON.stringify(unitContainer[i])));
@@ -156,13 +160,20 @@ function assignUnits(){                                                         
 
 }
 
-function battle(allTheUnits, p1, p2){                                                       // this function runs one step the battle by picking an attacking unit, a defending unit, and deadling damage
+function battleTurn(allTheUnits, p1, p2){                                                   // this function runs one turn the battle by picking an attacking unit, a defending unit, and deadling damage
 
     if(p1.length > 0 && p2.length > 0){                                                     // only run if both players still have a unit left
 
-        var attacker = allTheUnits[Math.floor(Math.random()*allTheUnits.length)];           // pick an attacking unit at random from the allTheUnits array
-        var defender;                                                                       // placeholder variables
+        var attackerID = allTheUnits[Math.floor(Math.random()*allTheUnits.length)];         // pick an attacking unit at random from the allTheUnits array
+        var attacker;                                                                       // these are placeholder variables
+        var defender;                                                                       
         var damage;
+
+        for(var j = 0; j < unitContainer.length; j++){                                      // cycle through the whole unitContainer array to find the unit with the corresponding ID
+            if (attackerID == unitContainer[j].id){                                         // make a "deep clone" of the unit - we can't just reference unitContainer[i], because then we'll end up making changes 
+                attacker = JSON.parse(JSON.stringify(unitContainer[j]));                    // to the unit template in the unitContainer array - which screws up multiple runs
+            }
+        }
 
         if (attacker.player == 1){
             defender = p2[Math.floor(Math.random()*p2.length)];                             // pick a defending unit from the opposing team
@@ -171,6 +182,8 @@ function battle(allTheUnits, p1, p2){                                           
         }
 
 /*      
+       ** uncomment to see the each battle turn **
+
         console.log("Attacker player: " + attacker.player + ", type " + attacker.type + ", id: " + attacker.id);
         console.log("Defender: player" + defender.player + ", type " + defender.type + ", id: " + defender.id);
         console.log("Attacker HP: " + attacker.hp);
@@ -182,9 +195,14 @@ function battle(allTheUnits, p1, p2){                                           
             damage += defender.armor;
         } 
 
+        if (damage <= 0 ){
+            damage = 1;
+        }
+
+
         defender.hp -= damage;                                                  // deal the damage  to the defender
 
-/*        console.log("Defender new HP: " + defender.hp);
+/*      console.log("Defender new HP: " + defender.hp);
         console.log("---");
 */
 
@@ -210,7 +228,7 @@ function battle(allTheUnits, p1, p2){                                           
         }
 
         if(p1.length > 0 && p2.length > 0){                                    // if both the p1 and p2 array still have units, run the function again until no units are left on one side     
-            battle(allTheUnits, p1, p2);
+            battleTurn(allTheUnits, p1, p2);
         } else {
             if(p1.length <= 0){                                                 // if one of the players runs out of units (loses the battle), increment the win counter for the other player
                 p2wins++;
@@ -240,15 +258,8 @@ function battle(allTheUnits, p1, p2){                                           
 }
 
 
-
-/* VIEW SETUP */
-
-
-
 /* RUN THE SIMULATION*/
 document.getElementById("run").addEventListener("click", function(){
-
-    console.log("Running is: " + running);
 
     if(!running){                                           // this prevents multiple launches
         runSimulation();
@@ -259,8 +270,6 @@ document.getElementById("run").addEventListener("click", function(){
 
 function runSimulation(){
 
-    console.log("Running is: " + running);
-
     runs = parseInt(document.getElementById("active-run").dataset.count);
 
     console.log("Running " + runs + " times.");
@@ -269,16 +278,16 @@ function runSimulation(){
     p1 = [];
     p2 = [];
     
-    if((playerUnitCount[0].count + playerUnitCount[1].count) > 0 && (playerUnitCount[2].count + playerUnitCount[3].count) > 0){         // make sure both players have units                            
+    if((playerUnitCount[0].count + playerUnitCount[1].count) > 0 && (playerUnitCount[2].count + playerUnitCount[3].count) > 0){         // check the count property playerUnitCount array to make sure both players have units                          
         
         running = true;
         document.getElementById("p1wins").classList.add("blur");
         document.getElementById("p2wins").classList.add("blur");
 
-        createUnits();
+        createUnits();                                                                            // create the temporary unit copies for this battle
 
 
-        for(var i = 0; i < runs; i++){                                                          // if both players have units, run the battle the preset number of times
+        for(var i = 0; i < runs; i++){                                                            // if both players have units, run the battle the preset number of times
             oneBattle(i);
         }
 
@@ -294,10 +303,14 @@ function oneBattle(i){
     setTimeout(function(){
 
         assignUnits();    
-        battle(allTheUnits, p1, p2);
+        battleTurn(allTheUnits, p1, p2);
     
     },((500/runs)*i))
 }
+
+
+/* VIEW CODE - the code that follows updates the view */
+
 
 
 var counts = document.getElementsByClassName("count");
@@ -335,7 +348,6 @@ for(var i = 0; i < decrements.length; i++){
 
 
 
-
 function changeStat(element, direction){
 
     var parent = element.parentNode.dataset.type
@@ -352,73 +364,73 @@ function changeStat(element, direction){
 
     switch(parent) {
         case "archer-strength":
-            if((archerStats.strength + change) > 0){
+            if((archerStats.strength + change) > 0 && (archerStats.strength + change) <= 10){
                 archerStats.strength += change;
                 document.getElementById(parent).innerHTML = archerStats.strength;
             }
             break;
         case "archer-armor":
-            if((archerStats.armor + change) > 0){
+            if((archerStats.armor + change) > 0 && (archerStats.armor + change) <= 10){
                 archerStats.armor += change;
                 document.getElementById(parent).innerHTML = archerStats.armor;
             }
             break;
         case "archer-speed":
-            if((archerStats.speed + change) > 0){
+            if((archerStats.speed + change) > 0 && (archerStats.speed + change) <= 10){
                 archerStats.speed += change;
                 document.getElementById(parent).innerHTML = archerStats.speed;
             }
             break;
         case "archer-hp":
-            if((archerStats.hp + change) > 0){
+            if((archerStats.hp + change) > 0 && (archerStats.hp + change) <= 10){
                 archerStats.hp += change;
                 document.getElementById(parent).innerHTML = archerStats.hp;
             }
             break;
         case "footman-strength":
-            if((footmanStats.strength + change) > 0){
+            if((footmanStats.strength + change) > 0 && (footmanStats.strength + change) <= 10){
                 footmanStats.strength += change;
                 document.getElementById(parent).innerHTML = footmanStats.strength;
             }
             break;
         case "footman-armor":
-            if((footmanStats.armor + change) > 0){
+            if((footmanStats.armor + change) > 0 && (footmanStats.armor + change) <= 10){
                 footmanStats.armor += change;
                 document.getElementById(parent).innerHTML = footmanStats.armor;
             }
             break;
         case "footman-speed":
-            if((footmanStats.speed + change) > 0){
+            if((footmanStats.speed + change) > 0 && (footmanStats.speed + change) <= 10){
                 footmanStats.speed += change;
                 document.getElementById(parent).innerHTML = footmanStats.speed;
             }
             break;
         case "footman-hp":
-            if((footmanStats.hp + change) > 0){
+            if((footmanStats.hp + change) > 0 && (footmanStats.hp + change) <= 10){
                 footmanStats.hp += change;
                 document.getElementById(parent).innerHTML = footmanStats.hp;
             }
             break;
         case "p1footmen":
-            if((playerUnitCount[0].count + change) >= 0){
+            if((playerUnitCount[0].count + change) >= 0 && (playerUnitCount[0].count + change) <= 15){
                 playerUnitCount[0].count += change;
                 document.getElementById(parent).innerHTML = playerUnitCount[0].count;
             }
             break;
         case "p2footmen":
-            if((playerUnitCount[2].count + change) >= 0){
+            if((playerUnitCount[2].count + change) >= 0 && (playerUnitCount[2].count + change) <= 15){
                 playerUnitCount[2].count += change;
                 document.getElementById(parent).innerHTML = playerUnitCount[2].count;
             }
             break;
         case "p1archers":
-            if((playerUnitCount[1].count + change) >= 0){
+            if((playerUnitCount[1].count + change) >= 0 && (playerUnitCount[1].count + change) <= 15){
                 playerUnitCount[1].count += change;
                 document.getElementById(parent).innerHTML = playerUnitCount[1].count;
             }
             break;
         case "p2archers":
-            if((playerUnitCount[3].count + change) >= 0){
+            if((playerUnitCount[3].count + change) >= 0 && (playerUnitCount[3].count + change) <= 15){
                 playerUnitCount[3].count += change;
                 document.getElementById(parent).innerHTML = playerUnitCount[3].count;
             }
@@ -533,13 +545,12 @@ document.getElementById("player-units-hint").addEventListener("mouseleave", func
 });
 
 
-var battleDescription1 = "How does an invidual battle work? First, each unit is added into an array based on their speed. For example, archers have a base speed of 5, so they're dropped into an array five times. "
-var battleDescription2 = "Next, a unit is picked from the array at random - but faster units have more copies in the array, so they're more likely to get picked. The selected unit is the attacker. " 
-var battleDescription3 = "A defending unit is picked at random from the opposing team. The attacking unit then deals damage equal to the attacker's strength minus the defender's armor (archers ignore armor and dead damage equal to their strength). "
-var battleDescription4 = "The defending unit takes damage, and is removed if HP drops to 0 or below. Then, a new attacking unit is picked at random, and the process continues until one player runs out of units, thereby losing the battle."
+var battleDescription1 = "How does an invidual battle work? <br><br>(1) First, each unit is added into a common array, once per unit of speed (Ex: archers have a base speed of 5, so they're dropped into an array five times.) "
+var battleDescription2 = "<br><br>(2) The attacking unit is picked from the array at random. A defending unit is also randomly selected from the opposing team. " 
+var battleDescription3 = "<br><br>(3) The attacking unit then deals damage equal to the attacker's strength minus the defender's armor (archers pierce armor and deal damage equal to their strength). "
+var battleDescription4 = "<br><br>(4) If a unit HP drops to 0, it is removed. This process continues until one player runs out of units, thereby losing the battle."
 
 var battleDescription = battleDescription1 + battleDescription2 + battleDescription3 + battleDescription4;
 
-console.log(battleDescription);
 
 
